77a01b182c13a49d83d55bac2d7deee5fd98626e
CodingGagan <gagandeep140197@gmail.com>
2021-08-26 00:16:00 +0530

    added login and registration pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages/index');
 });
+
+//Auth Routes
+Route::get('login', function () {
+    return view('auth/login');
+});
+
+Route::get('register', function () {
+    return view('auth/register');
+});
